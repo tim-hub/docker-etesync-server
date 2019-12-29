ce2acb452aa78c65670e343036957f1fbb1bf619
GRBurst <GRBurst@protonmail.com>
2019-12-29 01:24:24 +0100

    add database setting which respects DATA_DIR

--- context/etesync/etesync_site_settings.py
+++ context/etesync/etesync_site_settings.py
@@ -4,6 +4,13 @@ BASE_DIR = os.environ.get('BASE_DIR', '/etesync')
 
 DEBUG = bool(os.environ.get('DEBUG', False))
 
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.environ.get('DATA_DIR', os.path.join(BASE_DIR, 'db.sqlite3')),
+    }
+}
+
 ALLOWED_HOSTS = os.environ.get('DJANGO_HOSTS','*').split(',')
 
 LANGUAGE_CODE = os.environ.get('DJANGO_LC','en-us')
